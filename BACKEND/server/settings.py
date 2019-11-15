aa5a0984b2789e31cb551e3ae33bb0d87e36b2de
Anne <arianne.macena@gmail.com>
2019-11-14 23:26:23 -0300

    adds STATICFILES_DIRS variable in settings.py

--- BACKEND/server/settings.py
+++ BACKEND/server/settings.py
@@ -132,6 +132,9 @@ USE_TZ = True
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
