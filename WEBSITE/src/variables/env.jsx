16248f60afc3d0092219c8200914f3e643c3096c
Anne <arianne.macena@gmail.com>
2019-11-16 00:54:38 -0300

    updating front for heroku deploy

--- WEBSITE/src/variables/env.jsx
+++ WEBSITE/src/variables/env.jsx
@@ -1,7 +1,9 @@
 // #########################################
 //           enviroment variables
 // #########################################
-let BASE_URL = "http://127.0.0.1:8000"
+
+// let BASE_URL = "http://127.0.0.1:8000"
+let BASE_URL = "https://katchau-api.herokuapp.com/"
 
 export {
     BASE_URL
