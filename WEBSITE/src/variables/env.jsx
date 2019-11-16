2fd4088426073ea29b41db47dc32dc3754ff012a
Anne <arianne.macena@gmail.com>
2019-11-16 01:19:05 -0300

    testing deploy

--- WEBSITE/src/variables/env.jsx
+++ WEBSITE/src/variables/env.jsx
@@ -3,7 +3,7 @@
 // #########################################
 
 // let BASE_URL = "http://127.0.0.1:8000"
-let BASE_URL = "https://katchau-api.herokuapp.com/"
+let BASE_URL = "https://katchau-api.herokuapp.com"
 
 export {
     BASE_URL
